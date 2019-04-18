dc2d94a78d120f744888794df8f421a8e601f03a
RodrigoM586 <rocha.586@hotmail.com>
2019-04-18 16:29:37 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -117,8 +117,8 @@ html_theme_options = {
     'style_external_links': False,
     'vcs_pageview_mode': '',
     # Toc options
-    'collapse_navigation': False,
-    'sticky_navigation': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
     'navigation_depth': 3,
     'includehidden': True,
     'titles_only': False
